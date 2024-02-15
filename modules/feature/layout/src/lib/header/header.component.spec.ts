f962a1fbdcb1b2491a58fe3831123d79d221bccb
andrewarosario <andrew.arosario@gmail.com>
2024-02-14 21:04:02 -0300

    refactor: add title input to header

--- modules/feature/layout/src/lib/header/header.component.spec.ts
+++ modules/feature/layout/src/lib/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
+const mockTitle = 'Mock title';
+
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
@@ -12,6 +14,7 @@ describe('HeaderComponent', () => {
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    component.title = mockTitle;
     fixture.detectChanges();
   });
 
@@ -22,6 +25,10 @@ describe('HeaderComponent', () => {
   it('should contain title', () => {
     const header: HTMLHeadingElement =
       fixture.nativeElement.querySelector('header');
-    expect(header.textContent).toBe('Ecommerce');
+    expect(header.textContent).toBe(mockTitle);
+
+    component.title = 'Another title';
+    fixture.detectChanges();
+    expect(header.textContent).toBe('Another title');
   });
 });
--- modules/feature/layout/src/lib/header/header.component.ts
+++ modules/feature/layout/src/lib/header/header.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ecommerce-mentoria-2-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input({ required: true }) title = '';
+}
