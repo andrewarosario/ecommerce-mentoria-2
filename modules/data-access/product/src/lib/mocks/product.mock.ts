da37cddba6b497a643e5f5b348222cac1e8607f5
andrewarosario <andrew.arosario@gmail.com>
2024-02-21 21:17:54 -0300

    feat(product-data-access): add product search service

---
+++ modules/data-access/product/src/lib/mocks/product.mock.ts
@@ -0,0 +1,34 @@
+import { Product } from '../models/product.model';
+
+export const mockProducts: Product[] = [
+  {
+    createdAt: '2023-10-10T03:25:53.791Z',
+    name: 'Oriental Cotton Chicken',
+    price: '536.00',
+    description:
+      'New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016',
+    image: 'https://loremflickr.com/640/480/nature',
+    id: '1',
+    quantity: 1,
+  },
+  {
+    createdAt: '2023-10-09T20:45:06.372Z',
+    name: 'Recycled Cotton Shoes',
+    price: '927.00',
+    description:
+      'Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals',
+    image: 'https://loremflickr.com/640/480/fashion',
+    id: '2',
+    quantity: 1,
+  },
+  {
+    createdAt: '2023-10-10T06:46:25.914Z',
+    name: 'Licensed Metal Chair',
+    price: '304.00',
+    description:
+      'The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients',
+    image: 'https://loremflickr.com/640/480/food',
+    id: '3',
+    quantity: 1,
+  },
+];
---
+++ modules/data-access/product/src/lib/product-search/product-search.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductSearchService {
+  readonly apiUrl = 'https://65009f9718c34dee0cd53786.mockapi.io';
+
+  constructor(private http: HttpClient) {}
+
+  searchByName(name: string): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}/products`, {
+      params: { name },
+    });
+  }
+}
