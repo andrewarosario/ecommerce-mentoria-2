8db26a78567d3236a1a2279af58d5eb6ef3a534d
andrewarosario <andrew.arosario@gmail.com>
2024-03-20 20:46:06 -0300

    feat(home): set lazy loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NoPreloading, provideRouter, withPreloading } from '@angular/router';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withPreloading(NoPreloading)),
     provideHttpClient(),
     provideAnimationsAsync(),
   ],
