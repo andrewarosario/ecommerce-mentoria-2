e45057d144e07b0cc9c662f09606fcd2707d5066
andrewarosario <andrew.arosario@gmail.com>
2024-04-04 20:44:43 -0300

    feat(product-ui): add lib

---
+++ modules/ui/product/README.md
@@ -0,0 +1,7 @@
+# product-ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test product-ui` to execute the unit tests.
---
+++ modules/ui/product/src/lib/components/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ecommerce-mentoria-2-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+})
+export class ProductCardComponent {}
