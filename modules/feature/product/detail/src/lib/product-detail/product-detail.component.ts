cd4b0de8ee63d0f61d436953dc4e46a6b91fe7c8
andrewarosario <andrew.arosario@gmail.com>
2024-04-10 19:21:58 -0300

    feat(product-detail): add lib

---
+++ modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ecommerce-mentoria-2-product-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.scss',
+})
+export class ProductDetailComponent {}
