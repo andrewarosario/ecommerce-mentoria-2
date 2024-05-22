871c80834f85ef821e05d7c66f0eb1cde58c40ee
andrewarosario <andrew.arosario@gmail.com>
2024-05-22 20:59:49 -0300

    feat(product-detail): add cart integration

--- modules/data-access/product/src/index.ts
+++ modules/data-access/product/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/mocks/product.mock';
 export * from './lib/models/product.model';
 export * from './lib/product-search/product-search.service';
 export * from './lib/recommended-products/recommended-products.service';
+export * from './lib/state/cart/cart.service';
--- modules/ui/product/src/index.ts
+++ modules/ui/product/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/components/product-card/product-card.component';
+
+export * from './lib/components/cart/cart.component';
