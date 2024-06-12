6a2e5ee1df61521bbca1ffd37139790f306c1980
andrewarosario <andrew.arosario@gmail.com>
2024-06-12 20:00:28 -0300

    feat(auth-form): add lib

---
+++ modules/feature/auth/form/README.md
@@ -0,0 +1,7 @@
+# auth-form
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test auth-form` to execute the unit tests.
---
+++ modules/feature/auth/form/src/lib/auth-form/auth-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ecommerce-mentoria-2-auth-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-form.component.html',
+  styleUrl: './auth-form.component.scss',
+})
+export class AuthFormComponent {}
