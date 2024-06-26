3710ad9ece871f22422dda66b2c2e3eef12cbb66
andrewarosario <andrew.arosario@gmail.com>
2024-06-26 20:12:20 -0300

    feat(auth-form): integrates auth form

--- modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.spec.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
 import { AuthFormComponent } from '../auth-form.component';
 import { AuthFormEmailComponent } from './auth-form-email.component';
 
@@ -8,8 +10,8 @@ describe('AuthFormEmailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AuthFormEmailComponent],
-      providers: [AuthFormComponent],
+      imports: [AuthFormEmailComponent, NoopAnimationsModule],
+      providers: [AuthFormComponent, provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthFormEmailComponent);
@@ -19,5 +21,42 @@ describe('AuthFormEmailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should enable button when control is valid', () => {
+    const button: HTMLButtonElement =
+      fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBe(true);
+
+    component.control.setValue('user@mail.com');
+    fixture.detectChanges();
+
+    expect(button.disabled).toBe(false);
+  });
+
+  it('should display required error message', () => {
+    component.control.markAsTouched();
+    fixture.detectChanges();
+    const error: HTMLElement = fixture.nativeElement.querySelector(
+      '[data-testid="error-required"]'
+    );
+    expect(error).toBeTruthy();
+  });
+
+  it('should display email error message', () => {
+    // component.control.setValue('user');
+
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('input');
+    input.value = 'user';
+    input.dispatchEvent(new Event('input'));
+
+    component.control.markAsTouched();
+    fixture.detectChanges();
+
+    const error: HTMLElement = fixture.nativeElement.querySelector(
+      '[data-testid="error-email"]'
+    );
+    expect(error).toBeTruthy();
   });
 });
--- modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.ts
+++ modules/feature/auth/form/src/lib/auth-form/auth-form-email/auth-form-email.component.ts
@@ -1,11 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 import { AuthFormComponent } from '../auth-form.component';
 
 @Component({
   selector: 'ecommerce-mentoria-2-auth-form-email',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './auth-form-email.component.html',
   styleUrl: './auth-form-email.component.scss',
 })
